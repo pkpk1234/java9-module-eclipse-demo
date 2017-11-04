73b67a7ddda5000ac82a5d01ada0631e50156464
lijiaming <lijiaming1984@gmail.com>
2017-11-04 21:04:01 +0800

    add commnet,format codes

--- hello-module/com.example.hello/com/example/hello/HelloModule.java
+++ hello-module/com.example.hello/com/example/hello/HelloModule.java
@@ -1,5 +1,9 @@
 package com.example.hello;
 
+/**
+ * @author 李佳明 https://github.com/pkpk1234
+ * @date 2017-11-04
+ */
 public class HelloModule {
 	public static void main(String[] args) {
 		Class<HelloModule> clazz = HelloModule.class;
--- hello-requirer/com.example.requirer/com/example/requiere/RequirerMain.java
+++ hello-requirer/com.example.requirer/com/example/requiere/RequirerMain.java
@@ -2,6 +2,10 @@ package com.example.requiere;
 
 import com.example.hello.HelloModule;
 
+/**
+ * @author 李佳明 https://github.com/pkpk1234
+ * @date 2017-11-04
+ */
 public class RequirerMain {
 	public static void main(String[] args) {
 		HelloModule.main(args);
